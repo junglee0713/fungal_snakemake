89b03bb8f2172f50ab3e12c230df0617b932594a
Jung-Jin Lee <junglee0713@gmail.com>
2018-09-13 09:16:51 -0400

    Change run_snakemake.bash

--- run_snakemake.bash
+++ run_snakemake.bash
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-snakemake -j 80 --configfile /home/leej39/fungal_snakemake/config.yml --cluster-config /home/leej39/fungal_snakemake/cluster.json -w 90 --notemp -p -c "qsub -cwd -r n -V -l h_vmem={cluster.h_vmem} -l mem_free={cluster.mem_free} -pe smp {threads}" -n
+snakemake -j 80 \
+	--configfile /home/leej39/fungal_snakemake/config.yml \
+	--cluster-config /home/leej39/fungal_snakemake/cluster.json \
+	-w 90 \
+	--notemp \
+	-p \
+	-c \
+	"qsub -cwd -r n -V -l h_vmem={cluster.h_vmem} -l mem_free={cluster.mem_free} -pe smp {threads}" \
+	-n
